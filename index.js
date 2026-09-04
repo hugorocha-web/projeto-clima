1e1dbd0be653de9523432e20b15ad5cb777b44d7
hugorocha-web <hjj78092@gmail.com>
2026-09-04 16:53:14 -0300

    removi os consoles

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ async function pegarCord() {
     try{
         let infosCid = document.querySelector('input').value.toLowerCase().trim()
         let apichamada = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${infosCid},BR&limit=5&appid=ef17f7ce5ce0b6fb1684d90f66417048`)
-        console.log(apichamada)
+    
         if(!apichamada.ok){
             throw new Error('NÃO ACHEI A CIDADE')
         }
         let dados = await apichamada.json()
-        console.log(dados)
+    
         let latitude = dados[0].lat
         let longitude = dados[0].lon
         return {
@@ -31,7 +31,7 @@ btn.addEventListener('click', async () => {
 
     let latitude = coordenadas.latitude
     let longitude = coordenadas.longitude
-    console.log('aqui')
+    
     
     try{
         let apicidade = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ef17f7ce5ce0b6fb1684d90f66417048&units=metric&lang=pt_br`)
@@ -40,16 +40,16 @@ btn.addEventListener('click', async () => {
         }
         document.querySelector('#infos').style.display ='block'
         let dados = await apicidade.json()
-        console.log(dados)
+       
         let cidade = document.querySelector('#cidade')
         cidade.style.display = 'block'
         cidade.textContent = dados.name
         let graus = dados.main.temp
-        console.log(graus)
+        
         let situ = dados.weather[0].description
         let situacao = document.querySelector('#situação')
         situacao.style.display = 'block'
-        console.log(situ)
+        
         let infgraus = document.querySelector('#graus')
         infgraus.style.display = 'block'
         infgraus.textContent = Math.round(graus)+'°'
@@ -61,6 +61,7 @@ btn.addEventListener('click', async () => {
     }
     catch(error){
         console.log(error)
+        return null
     }
 
 
